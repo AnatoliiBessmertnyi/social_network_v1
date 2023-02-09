c46b237330b93043424864fd69a43b51910b6126
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-09 10:12:46 +0400

    Добавлен словарь контекста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,11 +6,20 @@ from django.shortcuts import render
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)  
+    title = 'Последние обновления на сайте'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)  
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)  
-
-
-
+    title = 'Лев Толстой – зеркало русской революции.'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)  
