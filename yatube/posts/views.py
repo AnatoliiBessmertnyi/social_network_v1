155ac83e8b36e77d98c30ccc7bc72bf89963ee9e
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-10 16:18:57 +0400

    Исправлено лого, добавлена БД

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,17 @@
-# ice_cream/views.py
-from django.http import HttpResponse
 from django.shortcuts import render
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
-
-# Главная страница
 def index(request):
-    template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
-    text = 'Это главная страница проекта Yatube'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': title,
-        'text': text,
+        'posts': posts,
     }
-    return render(request, template, context)  
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
