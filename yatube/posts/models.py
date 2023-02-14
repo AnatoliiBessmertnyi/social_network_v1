f12dc5f5e6454fa9fdfadf1134ffcd5ab48cdf0f
Анатолий Бессмертный <noob1917.ru@gmail.com>
2023-02-14 09:49:44 +0400

    Вывод информации из базы данных на страницы

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -25,3 +25,4 @@ class Group(models.Model):
     
     def __str__(self):
         return self.title
+#
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
+    # Одна строка вместо тысячи слов на SQL: 
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
     # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
